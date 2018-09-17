b3b9a39d2d0faa72226a9419a9eb36d71ba87a1e
RobertWCarey <rob_carey_1@hotmail.com>
2018-09-18 09:09:57 +1000

    added click button test

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 
 var webdriver = require('selenium-webdriver');
-
+var By = webdriver.By;
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 driver.get('http://movensw.com');
+
+driver.findElement(By.id('auth-signin-button')).click();
