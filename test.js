74df9aa533604e7e511d658e9e0c6b5c8c041fd6
RobertWCarey <rob_carey_1@hotmail.com>
2018-09-18 08:56:16 +1000

    npm init selenium install

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
-// var webdriver = require('selenium-webdriver');
-//
-// var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-//
-// driver.get('http://movensw.com');
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+
+driver.get('http://movensw.com');
