991afe6965b94305580b8b29447f0e55b09105bd
RobertWCarey <rob_carey_1@hotmail.com>
2018-09-18 13:32:43 +1000

    testing added heroku deployment

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ var By = webdriver.By;
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 driver.get('index.html');
-
+//
 driver.findElement(By.id('auth-signin-button')).click();
