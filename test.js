adf8663d50f5299eea9c4fb15296322a121830f0
RobertWCarey <rob_carey_1@hotmail.com>
2018-09-18 10:47:25 +1000

    start script added

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-driver.get('http://movensw.com');
+driver.get('index.html');
 
 driver.findElement(By.id('auth-signin-button')).click();
